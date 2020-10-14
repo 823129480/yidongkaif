d02f4fb0f366f3cb1b7958d92087c7633483d2dc
git key <823129480@qq.com>
2020-10-14 17:21:19 +0800

    no message

---
+++ Calculate/app/src/main/java/com/example/calculate/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.calculate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
